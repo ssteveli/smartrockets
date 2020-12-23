a4999e72757ede07bd2333939ee71fe2306a7f2a
Scott Stevelinck <scott@stevelinck.com>
2020-12-23 07:52:05 -0700

    dna

--- main.cpp
+++ main.cpp
@@ -15,14 +15,25 @@ class SmartRockets : public olc::PixelGameEngine
 {
 private:
     int numberOfRockets = 10;
+    int iterations = 200;
+    int frame = 0;
+
     olc::Sprite *sprRocket = NULL;
     olc::Decal *dRocket = NULL;
+
+    struct DNA
+    {
+        std::vector<olc::vf2d> genes;
+    };
 
     struct Rocket
     {
         olc::vf2d pos;
         olc::vf2d vel;
         olc::vf2d acc;
+        DNA dna;
+        bool completed;
+        bool crashed;
 
         void applyForce(olc::vf2d force)
         {
@@ -54,8 +65,16 @@ public:
         {
             Rocket rocket;
             rocket.pos = {ScreenWidth() / 2.0f, ScreenHeight() - 50.0f};
-            rocket.vel = {distr(eng), distr(eng)};
+            rocket.vel = {0.0f, 0.0f};
             rocket.acc = {0.0f, 0.0f};
+            rocket.completed = false;
+            rocket.crashed = false;
+
+            for (int n = 0; n < iterations; n++)
+            {
+                rocket.dna.genes.push_back({distr(eng), distr(eng)});
+            }
+
             rockets.push_back(rocket);
         }
 
@@ -68,14 +87,27 @@ public:
         SetPixelMode(olc::Pixel::ALPHA);
         for (auto &rocket : rockets)
         {
-            rocket.update();
+            if (frame++ >= iterations)
+                frame = 0;
+
+            if (rocket.pos.x > ScreenWidth() || rocket.pos.x < 0 || rocket.pos.y > ScreenHeight() || rocket.pos.y < 0)
+            {
+                rocket.crashed = true;
+                rocket.vel = {0.0f, 0.0f};
+            }
+
+            if (!rocket.completed || rocket.crashed)
+            {
+                rocket.applyForce(rocket.dna.genes[frame]);
+                rocket.update();
+            }
 
             DrawRotatedDecal(
                 rocket.pos,
                 dRocket,
                 atan2(rocket.vel.y, rocket.vel.x) + 0.90f);
         }
-
+        SetPixelMode(olc::Pixel::NORMAL);
         return true;
     }
 };
